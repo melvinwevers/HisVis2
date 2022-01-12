0ed85d5ecad3a7bbb90e48147f4a928627d03c68
melvinwevers <melvinwevers@gmail.com>
2022-01-12 14:29:29 +0100

    made proper code

---
+++ src/train_clip.py
@@ -0,0 +1,114 @@
+from fastai.vision.all import *
+import argparse
+
+import os
+import clip
+import torch
+
+import pandas as pd
+
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+import time
+
+
+from helper import *
+
+def main(training_path, param_sweep, output_path):
+
+    current_time = time.strftime("%Y%m%d")
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load('ViT-B/32', device)
+
+    # setting path variables 
+    Path.BASE_PATH = Path(training_path)
+    #Path.BASE_PATH.ls()
+    path = Path.BASE_PATH
+
+    data = get_dls(128, 224, path, augment=False)
+
+    # Calculate the image features
+    train_features, train_labels = get_features(model, data.train)
+    test_features, test_labels = get_features(model, data.valid)
+
+    # training logistic regression classifier
+    C = 0.65
+
+    if param_sweep:
+        C_param_range = [0.5, 0.55, 0.575, 0.6, 0.65, 0.7]
+
+        acc_Table = pd.DataFrame(columns = ['C_parameter','Accuracy'])
+        acc_Table['C_parameter'] = C_param_range
+
+        j = 0 
+        for i in C_param_range:
+            print(i)
+            classifier = LogisticRegression(random_state=0, C = i, max_iter=1000, n_jobs=-1)
+            classifier.fit(train_features, train_labels)
+        
+            predictions = classifier.predict(test_features)
+            
+            acc_Table.iloc[j, 1] = np.mean((test_labels == predictions).astype(np.float)) * 100.
+            
+            j += 1
+        
+        C = acc_Table[acc_Table['Accuracy'] == acc_Table['Accuracy'].max()]['C_parameter'].values
+
+
+    # Perform logistic regression
+    classifier = LogisticRegression(random_state=0, C=C, max_iter=1000, verbose=0, n_jobs=-1)
+    classifier.fit(train_features, train_labels)
+
+    # Evaluate using the logistic regression classifier
+    predictions = classifier.predict(test_features)
+    accuracy = np.mean((test_labels == predictions).astype(np.float)) * 100.
+    print(f"Accuracy = {accuracy:.3f}")
+
+    test_labels_ = [data.vocab[x] for x in test_labels]
+    predictions_ = [data.vocab[x] for x in predictions]
+
+    print(classification_report(test_labels_, predictions_))
+
+    report = classification_report(test_labels_, predictions_, output_dict=True)
+    report_df = pd.DataFrame(report).transpose()
+    filename = current_time + '_classification_reportCLIP.csv'
+    report_df.to_csv(os.path.join(output_path, filename))
+
+    ## Calculate top-5 accuracy
+    correct = []
+    pred_5 = [] 
+
+    top5 = 0.0 
+
+    probs = classifier.predict_proba(test_features)
+    best_n = np.argsort(probs, axis=1)[:,-5:]
+
+    for i, preds in enumerate(best_n):
+        if test_labels[i] in best_n[i]:
+            top5 += 1.0
+
+    print("top5 acc", top5/len(best_n)) 
+
+
+    # Save Model
+    filename = current_time + '_clip_linear_prob_model.sav'
+    pickle.dump(classifier, open(os.path.join(output_path, filename), 'wb'))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--training_data_path', type=str, default='../../../news_nl')
+    parser.add_argument('--param_sweep', type=bool, default=False)
+    parser.add_argument('--output_path', type=str, default='./output/models')
+    args = parser.parse_args()
+
+    if not os.path.exists('./output/models'):
+        os.makedirs('./output/models')
+    
+    main(args.training_data_path, args.param_sweep, args.output_path)
---
+++ src/train_places_model.py
@@ -0,0 +1,102 @@
+
+import argparse
+from fastai.vision.all import *
+from torch.serialization import default_restore_location
+import torchvision.models as models
+from sklearn.metrics import classification_report
+
+from helper import *
+
+
+def load_places():
+    # th architecture to use
+    arch = 'resnet50'
+
+    # (down)load the pre-trained weights
+    model_weights = '%s_places365.pth.tar' % arch
+    if not os.access(model_weights, os.W_OK):
+        weight_url = 'http://places2.csail.mit.edu/models_places365/' + model_weights
+        os.system('wget ' + weight_url)
+        
+    places_res50 = torch.load(model_weights,  
+                            map_location=lambda storage, 
+                            loc: storage)
+
+    default_res50 = models.resnet50()
+    state_dict = places_res50['state_dict'] 
+    new_state_dict = OrderedDict()
+
+    for key in state_dict.keys():
+        new_state_dict[key[7:]]= state_dict[key]
+        
+
+    default_res50.fc = torch.nn.Linear(2048, 365) # Matching with default res50 dense layer
+    default_res50.load_state_dict(new_state_dict)
+    return default_res50
+
+def export_classification_report(learn, output_path, current_time):
+    interp = ClassificationInterpretation.from_learner(learn)
+    interp.print_classification_report()
+
+    predictions_,test_labels_ = flatten_check(interp.decoded, interp.targs)
+    report = classification_report(test_labels_, predictions_, 
+                                   labels=list(interp.vocab.o2i.values()), 
+                                   target_names=[str(v) for v in interp.vocab],
+                                   output_dict=True)
+    report_df = pd.DataFrame(report).transpose()
+    filename = current_time + '_classification_reportPlaces.csv'
+    report_df.to_csv(os.path.join(output_path, filename))
+
+
+
+def new_resnet(pretrained):
+    '''
+    fix for cnn_learner module using non-standard pre-trained models
+    '''
+    return default_restore_location
+
+
+
+def main(training_path, n_epochs, lr, output_path):
+
+    current_time = time.strftime("%Y%m%d")
+
+    # setting path variables 
+    Path.BASE_PATH = Path(training_path)
+    #Path.BASE_PATH.ls()
+    path = Path.BASE_PATH
+
+    data = get_dls(128, 224)
+    classes = data.vocab
+
+    default_res50 = load_places()
+
+    learn = cnn_learner(get_dls(128, 224, augment=True),
+                    new_resnet,
+                    #models.resnet34,
+                    metrics=[top_k_accuracy, 
+                             accuracy,
+                             error_rate])
+
+    # train model
+    learn.fit_one_cycle(n_epochs, lr, cbs=[EarlyStoppingCallback(monitor='valid_loss', min_delta=0.01, patience=5)])
+
+    # evaluate model
+    export_classification_report(learn, output_path, current_time)
+
+    # save model
+    filename = current_time + 'DeBoerPlaces.pkl'   
+    learn.export(os.path.join(output_path, filename))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--training_data_path', type=str, default='../../../news_nl')
+    parser.add_argument('--n_epochs', type=int, default=50)
+    parser.add_argument('--lr', type=float, default=3e-3)
+    parser.add_argument('--output_path', type=str, default='./output/models')
+    args = parser.parse_args()
+
+    if not os.path.exists('./output/models'):
+        os.makedirs('./output/models')
+    
+    main(args.training_data_path, args.n_epochs, args.lr, args.output_path)
